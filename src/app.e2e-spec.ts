86ab4ac941609b7b26bf8cb9b257798f61673678
Torsten Blasche <torsten.blasche@obi.de>
2020-10-26 16:41:55 +0100

    Moved from express to fastify

--- src/app.e2e-spec.ts
+++ src/app.e2e-spec.ts
@@ -1,24 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return app.inject({ method: 'GET', url: '/' })
+      .then(({ statusCode, headers, payload }) => {
+        expect(statusCode).toBe(200);
+        expect(payload).toEqual('Hello World!');
+      });
   });
 });
--- src/metrics/metrics.controller.e2e-spec.ts
+++ src/metrics/metrics.controller.e2e-spec.ts
@@ -1,27 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import MetricsModule from './metrics.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 describe('MetricsController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [MetricsModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
     await app.init();
   });
 
   it('GET /metrics should return metrics', () => {
-    return request(app.getHttpServer())
-      .get('/metrics')
-      .expect(res => {
-        expect(res.status).toBe(200);
-        expect(res.headers['content-type']).toMatch(/^text\/plain; charset=utf-8; version=/);
-        expect(res.text).toContain('nodejs_version_info');
+    return app.inject({ method: 'GET', url: '/metrics' })
+      .then(({ statusCode, headers, payload }) => {
+        expect(statusCode).toBe(200);
+        expect(headers['content-type']).toMatch(/^text\/plain; /);
+        expect(headers['content-type']).toContain("charset=utf-8");
+        expect(payload).toContain('nodejs_version_info');
       });
   });
 });
