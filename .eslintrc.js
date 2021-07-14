8a5dfc858c438ed383c82c45d4f93d3fbc478fbe
Torsten Blasche <torsten.blasche@obi.de>
2021-07-14 14:45:16 +0200

    Simplified eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'plugin:import/warnings'
+    'plugin:@typescript-eslint/recommended'
   ],
   root: true,
   env: {
@@ -20,12 +16,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': ['error', { allowArgumentsExplicitlyTypedAsAny: true }],
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-    '@typescript-eslint/quotes': ['error', 'single'],
-    'comma-dangle': ['error', 'never'],
-    'import/order': 'error',
-    'semi': ['error', 'always'],
-    'sort-imports': ['error', { ignoreDeclarationSort: true }]
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }]
   }
 };
